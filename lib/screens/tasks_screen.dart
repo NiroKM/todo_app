448438530db6170c31c4775b0f67240f7982f822
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2021-05-11 21:59:08 +0530

    creating home page layout

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -3,9 +3,65 @@ import 'package:flutter/material.dart';
 class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('this is the main widget'),),
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(bottom: 20.0, right: 20.0),
+        child: FloatingActionButton(
+          backgroundColor: Colors.lightBlueAccent,
+          child: Icon(
+            Icons.add,
+            size: 35.0,
+          ),
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.only(top: 60.0, right: 30.0, left: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30.0,
+                    child: Icon(
+                      Icons.list,
+                      size: 30.0,
+                      color: Colors.lightBlueAccent,
+                    )),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                      fontSize: 50.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w700),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0))),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
