d734397ba464ab7d9a31ea01b717b975cd50090a
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2021-05-11 12:25:47 +0530

    initial git commit

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('this is the main widget'),),
+    );
+  }
+}
